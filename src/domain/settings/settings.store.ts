1286de741ce1524402f3e84fcafffd5224d2a772
alexisvasseur <alexis.vasseur@eurecia.com>
2025-08-04 10:11:20 +0200

    feat(souszone): added souszone filter

--- src/domain/settings/settings.store.ts
+++ src/domain/settings/settings.store.ts
@@ -2,8 +2,8 @@ import { defineStore } from 'pinia'
 import { ref } from 'vue'
 import type { Settings } from './settings.types'
 
-const API_KEY = '03420c-ba4a3d-3fd214-19c562-41ae9e'
-const EDIT_KEY = '5a3443-b6c062-bb22f7-19a24d-e957d7'
+const API_KEY = ''
+const EDIT_KEY = ''
 const DEFAULT_PSEUDO = 'zoph' // Set a default pseudo if needed
 
 export const useSettingsStore = defineStore('settings', () => {
