eb1eae7e9f3910afb52bcadd86d3d93767fc5b47
alexisvasseur <alexis.vasseur@eurecia.com>
2025-08-04 09:07:44 +0200

    feat(souszone): added souszone filter

--- src/domain/settings/settings.store.ts
+++ src/domain/settings/settings.store.ts
@@ -1,30 +1,27 @@
-import { defineStore } from "pinia";
-import { ref } from "vue";
-import type { Settings } from "./settings.types";
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
+import type { Settings } from './settings.types'
 
-
-const API_KEY = ""
-const EDIT_KEY = ""
-const DEFAULT_PSEUDO = "zoph"; // Set a default pseudo if needed
+const API_KEY = '03420c-ba4a3d-3fd214-19c562-41ae9e'
+const EDIT_KEY = '5a3443-b6c062-bb22f7-19a24d-e957d7'
+const DEFAULT_PSEUDO = 'zoph' // Set a default pseudo if needed
 
 export const useSettingsStore = defineStore('settings', () => {
-
   const settings = ref<Settings>({
     apiKey: localStorage.getItem('apiKey') || API_KEY,
     userKey: localStorage.getItem('userKey') || EDIT_KEY,
-    pseudo: localStorage.getItem('pseudo') || DEFAULT_PSEUDO
-  });
+    pseudo: localStorage.getItem('pseudo') || DEFAULT_PSEUDO,
+  })
 
   function updateSettings(newSettings: Settings) {
-    localStorage.setItem('apiKey', newSettings.apiKey);
-    localStorage.setItem('userKey', newSettings.userKey);
-    localStorage.setItem('pseudo', newSettings.pseudo);
-    settings.value = newSettings;
+    localStorage.setItem('apiKey', newSettings.apiKey)
+    localStorage.setItem('userKey', newSettings.userKey)
+    localStorage.setItem('pseudo', newSettings.pseudo)
+    settings.value = newSettings
   }
 
   return {
     settings,
-    updateSettings
-  };
-
+    updateSettings,
+  }
 })
